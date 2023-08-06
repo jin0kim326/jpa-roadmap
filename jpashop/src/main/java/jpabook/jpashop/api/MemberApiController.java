4ba31030828ae46148f7fbe42c7cde6cc15351ad
김진영 <jin0kim326@douzone.com>
2023-08-07 08:51:23 +0900

    [START] #2.1 회원등록 API  - 엔티티 노출 금지!

---
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    /**
+     * V2
+     * Member 엔티티가 변경되어도 API 스펙에는 전혀 영향없음
+     */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class CreateMemberResponse {
+        private Long id;
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+}
